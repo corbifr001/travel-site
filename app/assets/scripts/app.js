4a338ca68bd3b8bafdbe618af1102b726595eb6e
Francis Corbin <francis.corbin@prysmiangroup.com>
2018-03-04 14:17:28 -0500

    Reveal Complete

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,3 +1,8 @@
+import $ from 'jquery';
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/reveal-on-scroll';
 
 var mobileMenu = new MobileMenu();
+
+var featureItems = new RevealOnScroll($(".feature-item"), "85%");
+var testimonials = new RevealOnScroll($(".testimonial"), "60%");
