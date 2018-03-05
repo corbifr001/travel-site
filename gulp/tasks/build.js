70bcfe7437da4a4f119ce285b48909527d372e00
Francis Corbin <francis.corbin@prysmiangroup.com>
2018-03-05 12:45:30 -0500

    Build task complete 2

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -11,13 +11,13 @@ gulp.task('previewDist', function() {
     browserSync.init({
 		notify: false,
 		server: {
-			baseDir: "app"
+			baseDir: "docs"
 		}
 	});
 });
 
 gulp.task('deleteDistFolder', function() {
-    return del('./dist');
+    return del('./docs');
 });
 
 gulp.task('optimizeImages', ['deleteDistFolder', 'icons'], function() {
@@ -31,7 +31,7 @@ gulp.task('optimizeImages', ['deleteDistFolder', 'icons'], function() {
         interlaced: true,
         multipass: true
     }))
-    .pipe(gulp.dest("./dist/assets/images"));
+    .pipe(gulp.dest("./docs/assets/images"));
 });
 
 gulp.task('usemin', ['deleteDistFolder', 'styles', 'scripts'], function() {
@@ -40,7 +40,7 @@ gulp.task('usemin', ['deleteDistFolder', 'styles', 'scripts'], function() {
             css: [function() { return rev(); }, function() { return cssnano(); }],
             js: [function() { return rev(); }, function() { return uglify(); }]
         }))
-        .pipe(gulp.dest('./dist'));
+        .pipe(gulp.dest('./docs'));
 });
 
 gulp.task('build', ['deleteDistFolder', 'optimizeImages', 'usemin']);
