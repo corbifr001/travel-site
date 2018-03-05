98b4dae8a0e6cccc4f3012d24be84b90d273949f
Francis Corbin <francis.corbin@prysmiangroup.com>
2018-03-04 21:55:30 -0500

    Working on modal window

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -2,10 +2,13 @@ import $ from 'jquery';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/reveal-on-scroll';
 import StickyHeader from './modules/StickyHeader';
+import Modal from './modules/Modal';
 
 var mobileMenu = new MobileMenu();
 
 var featureItems = new RevealOnScroll($(".feature-item"), "85%");
 var testimonials = new RevealOnScroll($(".testimonial"), "60%");
 
-var stickyHeader = new StickyHeader();
+var stickyHeader = new StickyHeader();  
+
+var modal = new Modal();
