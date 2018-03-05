a3bf95053abcddf466779c871441d8fdf8954bf6
Francis Corbin <francis.corbin@prysmiangroup.com>
2018-03-04 20:11:46 -0500

    Finish sticky-header

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,8 +1,11 @@
 import $ from 'jquery';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/reveal-on-scroll';
+import StickyHeader from './modules/StickyHeader';
 
 var mobileMenu = new MobileMenu();
 
 var featureItems = new RevealOnScroll($(".feature-item"), "85%");
 var testimonials = new RevealOnScroll($(".testimonial"), "60%");
+
+var stickyHeader = new StickyHeader();
